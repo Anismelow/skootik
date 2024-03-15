3bdbe1bb9cfb1f8351aebb8fd2ed3fa067303ae7
anismelow <anyellmendozalopez@gmail.com>
2024-03-15 10:43:59 +0100

    cambio valor de foto

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -44,7 +44,7 @@ function UserList({ users }) {
       {users.length > 0 ? (
         users.map((user, index) => (
           <div key={index} className="max-w-sm rounded overflow-hidden shadow-lg m-4">
-            <img src={user.picture.thumbnail} alt={user.name.first} className="w-full" />
+            <img src={user.picture.large} alt={user.name.first} className="w-full" />
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">
                 {user.name.first} {user.name.last}
