a9f0c302173393dd454a322cd994dd6469ba375a
anismelow <anyellmendozalopez@gmail.com>
2024-03-15 09:39:35 +0100

    se agrego useEfect

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,39 @@
-import React, { useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
 import UserSearchForm from './components/UserSearchForm';
 import UserList from './components/UserList';
 
 function App() {
   const [manyUsers, setManyUsers] = useState([]);
 
-  const handleSearchSubmit = async ({ numUser, gender, nationality }) => {
+  const fetchUsers = async ({ numUser, gender, nationality }) => {
     const URL_API_USERS = `https://randomuser.me/api/?results=${numUser}&gender=${gender}&nat=${nationality}&page=1`;
     try {
       const response = await fetch(URL_API_USERS);
       const data = await response.json();
-      setManyUsers(data.results);
+      return data.results;
     } catch (error) {
       console.error('Error fetching users:', error);
+      return [];
     }
   };
 
+  const handleSearchSubmit = async (searchParams) => {
+    const users = await fetchUsers(searchParams);
+    setManyUsers(users);
+  };
+
+  useEffect(() => {
+    handleSearchSubmit({});
+
+  }, []); // Sin dependencias
+
   return (
-    <>
-      <div className="container mx-auto p-4">
-        <h1 className="text-2xl font-bold mb-4 text-center">Random Users</h1>
-        <UserSearchForm onSubmit={handleSearchSubmit} />
-        <UserList users={manyUsers} />
-      </div>
-    </>
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4 text-center">Random Users</h1>
+      <UserSearchForm onSubmit={handleSearchSubmit} />
+      <UserList users={manyUsers} />
+    </div>
   );
 }
 
